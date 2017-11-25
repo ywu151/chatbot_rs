06b3ac27d30dca8b827dd0d801d9ab89a778f4fd
ywu <313621907@qq.com>
2017-11-25 11:39:40 -0800

    tmp

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('HEY!')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+});
+ 
+app.post('/get_story', urlencodedParser, function (req, res) {
+   console.log('StoryId: ', storyId);
+   var mysql      = require('mysql');
+   var connection = mysql.createConnection({
+       host     : 'slugchat-test.lorabit.com',
+       user     : 'root',
+       password : 'password',
+       database : 'slugchat'
+   });
+
+   connection.connect();
+   var storyId = Math.floor(Math.random() * 700) + 1; 
+
+   var content = "没有找到合适的故事";
+   connection.query('SELECT * FROM slugchat.tbl_stories WHERE storyId = ' + storyId, function (error, results, fields) {
+       if (error) throw error;
+       console.log('StoryId: ', storyId);
+       content = results[0]['title'] + '\n' + results[0]['content'];
+       console.log('Content: ', content);
+       var response = {
+           'content': content
+       };
+       console.log(response);
+       res.send(content);
+   });
 })
-
-app.listen(3000, () => console.log('Server running on port 3000'))
+ 
+var server = app.listen(3000, () => console.log('Server running on port 3000'))
